7c91854f55f6df7fbcd446ec67c78ada9120a0ee
vikal kumar <vikalkumar1591@gmail.com>
2021-08-15 11:58:57 +0530

    Sliding window problems

---
+++ sw/1.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+// int main()
+// {
+//     int arr[]={7,5,4,6,8,9};
+//     int i=0,j=0;
+//     int k=3;
+//     int x=20;
+//     int ans=0;
+//     int sum=0;
+//     while (j<6)
+//     {
+//         sum+=arr[j];
+//         if((j-i+1)<k){
+//          j++;
+//         }
+//        else if((j-i+1)==k)
+//         {
+//            if(sum < x){
+//                ans=max(ans,sum);
+//            }
+//            sum-=arr[i];
+//            j++;
+//            i++;
+//         }
+//     }
+//     cout<<ans;  
+// }
+
